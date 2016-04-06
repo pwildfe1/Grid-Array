b6bea1fe067fe79c0248257473bc6752c485d184
Peter Wildfeuer <wildfeuer.peter@gmail.com>
2016-04-06 18:08:51 -0400

    maintained interior linkages and tried adding multi-dimensionality

--- LatticeUnitStablize.py
+++ LatticeUnitStablize.py
@@ -17,7 +17,7 @@ class cell:
             subPts.append(subPt)
         piece = subCell(self.vert[origin],subPts,self.ratio)
         return piece
-    def genCell(self):
+    def genCell(self,attPts,thres,maxRatio):
         mySubCells = []
         members = []
         mySubCells.append(self.genSubCell(0,[1,3,4]))
@@ -29,7 +29,8 @@ class cell:
         mySubCells.append(self.genSubCell(6,[7,5,2]))
         mySubCells.append(self.genSubCell(7,[6,4,3]))
         for i in range(len(mySubCells)):
-            members.append(mySubCells[i].gen())
+            newOrder = mySubCells[i].warpSelf(attPts,thres,maxRatio)
+            members.append(mySubCells[i].form(newOrder))
         return members
 
 class subCell:
@@ -47,9 +48,19 @@ class subCell:
                 if i!=j:
                     self.vert[i] = rs.PointAdd(self.vert[i],self.axes[j]*self.ratio[j])
         for i in range(len(self.vert)):
-            members.append(rs.AddLine(self.origin,self.vert[i]))
-        members.append(rs.AddPolyline(self.vert))
+            members.append([self.origin,self.vert[i]])
+        members.append(self.vert)
         return members
+    def warpSelf(self,attPts,thres,maxRatio):
+        members = self.gen()
+        for i in range(len(members)):
+            members[i] = warp(members[i],attPts,thres,maxRatio)
+        return members
+    def form(self,members):
+        crvs = []
+        for i in range(len(members)):
+            crvs.append(rs.AddPolyline(members[i]))
+        return crvs
 
 def grid(x,y,z,dim,attPts,thres):
     newObjs = []
@@ -58,6 +69,9 @@ def grid(x,y,z,dim,attPts,thres):
         for j in range(dim):
             for k in range(dim):
                 pts.append([x*i,y*j,z*k])
+    return pts
+
+def warp(pts,attPts,thres,maxRatio):
     for i in range(len(pts)):
         vecSum = [0,0,0]
         sum = 0
@@ -69,23 +83,24 @@ def grid(x,y,z,dim,attPts,thres):
                 vecSum = rs.PointAdd(vecSum,vec)
                 sum = sum+dist/thres
                 num = num+1
-            if num>0:
-                val = 1-sum/num
-                if val>.5:
-                    val = .5
-                pts[i] = rs.PointAdd(pts[i],val*vecSum)
+        if num>0:
+            val = 1-sum/num
+            if val>maxRatio:
+                val = maxRatio
+            vecSum = vecSum/num
+            pts[i] = rs.PointAdd(pts[i],val*vecSum)
     return pts
-
 
 def Main():
     objs = rs.GetObjects("select objects")
     attPts = rs.GetObjects("select attPts",rs.filter.point)
     box = rs.BoundingBox(objs)
-    dim = 8
+    dim = 6
     X = rs.GetReal("please enter x axis spacing",rs.Distance(box[1],box[0]))
     Y = rs.GetReal("please enter y axis spacing",rs.Distance(box[0],box[3]))
     Z = rs.GetReal("please enter z axis spacing",rs.Distance(box[0],box[4]))
     thres = rs.GetReal("please enter attractor strength",(X+Y+Z)/3*4)
+    maxRatio = rs.GetReal("please enter maximum ratio",.5)
     gridPts = grid(X,Y,Z,dim,attPts,40)
     for i in range(len(gridPts)-dim*dim-1):
         cellPts = [gridPts[i],gridPts[i+dim],gridPts[i+dim*dim+dim],gridPts[i+dim*dim]] 
@@ -98,7 +113,7 @@ def Main():
                     max=dist
         if max<X+Y+Z:
             myCell = cell(cellPts,[.5,0,.5])
-            members = myCell.genCell()
+            members = myCell.genCell(attPts,thres,maxRatio)
         cellPts = []
     return dim
 
